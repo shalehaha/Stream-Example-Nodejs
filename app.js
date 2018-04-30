739e5a81811b4f500ba80b1db51369bc55ba048b
jgeng <jgeng@adobe.com>
2018-04-29 20:55:52 -0600

    dev

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ var bodyParser = require('body-parser'),
 	passport = require('passport'),
 	passportConfig = require('./config/passport'),
 	passport_mock = require('./passport_mock'),
+	getstream = require('getstream'),
 	routes = require('./routes');
 
 var app = express();
@@ -33,5 +34,36 @@ app.use(passport.session());
 app.use('/', routes);
 app.use(express.static(__dirname + '/static'));
 app.use('/media', express.static(__dirname + '/media'));
+
+
+app.get('/userToken', function(req, res) {
+
+const client = stream.connect('9uw4aec6qkym', 'kexftsstxwf8fjvwfgpnjszz8jvnz338yhtry5jwvbx99as35qugwms5x3nn4f4x', '36857');
+
+    let name = req.query.name;
+const token = client.feed('user', name).token
+	res.send(JSON.stringify({token:token}));
+
+
+});
+
+app.get('/followUser', function(req, res) {
+    console.log(req.body);
+    pingpp.charges.create({
+        subject: req.body.subject,
+        body: req.body.body,
+        amount: req.body.amount,
+        order_no: req.body.order_no,
+        channel: req.body.channel,
+        currency: "cny",
+        client_ip: "127.0.0.1",
+        app: { id: "app_jX5abPCu98iLyLGW" }
+    }, function(err, charge) {
+        console.log(charge);
+        console.log(err);
+        res.send(JSON.stringify(charge));
+    });
+});
+
 
 app.listen(config.get('PORT'));
